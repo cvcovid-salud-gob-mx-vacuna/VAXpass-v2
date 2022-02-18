9dcbc5b9b66a637c239e30ed922f6641fd7569bb
Sam Rahimi <samrahimi@Sams-MacBook-Pro.local>
2022-02-17 23:38:41 -0600

    gitignore

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Generate PDF and return the link */
+router.get('/pdf', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
